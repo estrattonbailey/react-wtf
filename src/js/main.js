f9ed74214e758d93b7fd2d84b99606975b66db75
estrattonbailey <ericstrattonbailey@gmail.com>
2016-11-03 11:40:22 -0400

    Add log

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import Prez from './components'
 
+console.groupCollapsed('😎')
+console.log(`Built by https://github.com/estrattonbailey who probably shouldn't even be allowed to present on this.`)
+console.log(`Source: https://github.com/estrattonbailey/vim-lol`)
+console.groupEnd()
+
 render(<Prez/>, document.getElementById('root'))
